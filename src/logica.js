1ce7a84f42045fe1982ee8bfc598f3641de697c4
LUIZ VIEIRA <beat.luiz23@hotmail.com>
2021-06-19 11:02:58 -0300

    att

--- src/logica.js
+++ src/logica.js
@@ -3,11 +3,13 @@ function mostrar_tutoriais() {
     tutorial = document.getElementById('id_tutoriais')
     site = document.getElementById('id_site')
     portfolio = document.getElementById('id_portfolio')
+    inicio = document.getElementById('inicio')
 
 
     tutorial.style.display = "block";
-    site.style.display = "none";
+    // site.style.display = "none";
     portfolio.style.display = "none";
+    inicio.style.display = "none";
 
 }
 
@@ -15,10 +17,12 @@ function mostrar_site() {
     tutorial = document.getElementById('id_tutoriais')
     site = document.getElementById('id_site')
     portfolio = document.getElementById('id_portfolio')
-
-    site.style.display = "block";
+    inicio = document.getElementById('inicio')
+        // site.style.display = "block";
     tutorial.style.display = "none";
     portfolio.style.display = "none";
+    inicio.style.display = "none";
+
 }
 
 function mostrar_portfolio() {
@@ -26,10 +30,11 @@ function mostrar_portfolio() {
     tutorial = document.getElementById('id_tutoriais')
     site = document.getElementById('id_site')
     portfolio = document.getElementById('id_portfolio')
-
+    inicio = document.getElementById('inicio')
     portfolio.style.display = "block";
-    site.style.display = "none";
+    // site.style.display = "none";
     tutorial.style.display = "none";
+    inicio.style.display = "none";
 
 }
 
