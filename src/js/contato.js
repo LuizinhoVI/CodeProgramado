1f35cde1dc08c42bb63d7e3b3ac70c94aad360bc
LUIZ VIEIRA <beat.luiz23@hotmail.com>
2021-06-26 12:47:24 -0300

    att

--- src/js/contato.js
+++ src/js/contato.js
@@ -1,51 +1,40 @@
 var phone = document.getElementsByClassName("barra-ok");
 //phone.style.background = "blue";
 
-if (phone == "blue") {
-
-    alert("")
-}
 
 
 function cell() {
-    phone[0].style.background = "blue";
-
-
-    if (phone[0] == "blue") {
-        alert("testado ")
-    }
-
+    var t = [phone[0].style.background = "blue", phone[1].style.background = "none", phone[2].style.background = "none"]
 
 }
 
 function pc() {
-    phone[1].style.background = "blue";
+    var t = [phone[0].style.background = "none", phone[1].style.background = "blue", phone[2].style.background = "none"]
 
 }
 
 function web() {
-    phone[2].style.background = "blue";
+    var t = [phone[0].style.background = "none", phone[1].style.background = "none", phone[2].style.background = "blue"]
 
 }
 
 // funções das escolhas de contatos 
 
 function email() {
-    phone[3].style.background = "blue";
+    var t = [phone[3].style.background = "blue", phone[4].style.background = "none", phone[5].style.background = "none", phone[6].style.background = "none"]
 
 }
 
 function inst() {
-    phone[4].style.background = "blue";
-
+    var t = [phone[3].style.background = "none", phone[4].style.background = "blue", phone[5].style.background = "none", phone[6].style.background = "none"]
 }
 
 function telegram() {
-    phone[5].style.background = "blue";
+    var t = [phone[3].style.background = "none", phone[4].style.background = "none", phone[5].style.background = "blue", phone[6].style.background = "none"]
 
 }
 
 function wapp() {
-    phone[6].style.background = "blue";
+    var t = [phone[3].style.background = "none", phone[4].style.background = "none", phone[5].style.background = "none", phone[6].style.background = "blue"]
 
 }
