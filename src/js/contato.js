afb3f26dcc02286a0cf069bbbb8b662db1ccf654
LUIZ VIEIRA <beat.luiz23@hotmail.com>
2021-06-26 01:40:22 -0300

    atualização1.0

---
+++ src/js/contato.js
@@ -0,0 +1,51 @@
+var phone = document.getElementsByClassName("barra-ok");
+//phone.style.background = "blue";
+
+if (phone == "blue") {
+
+    alert("")
+}
+
+
+function cell() {
+    phone[0].style.background = "blue";
+
+
+    if (phone[0] == "blue") {
+        alert("testado ")
+    }
+
+
+}
+
+function pc() {
+    phone[1].style.background = "blue";
+
+}
+
+function web() {
+    phone[2].style.background = "blue";
+
+}
+
+// funções das escolhas de contatos 
+
+function email() {
+    phone[3].style.background = "blue";
+
+}
+
+function inst() {
+    phone[4].style.background = "blue";
+
+}
+
+function telegram() {
+    phone[5].style.background = "blue";
+
+}
+
+function wapp() {
+    phone[6].style.background = "blue";
+
+}
