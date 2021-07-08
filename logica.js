387cb43fad65fd72fa1db9d1b1bc3ba47c3f1afa
LUIZ VIEIRA <beat.luiz23@hotmail.com>
2021-07-08 02:19:14 -0300

    att

---
+++ logica.js
@@ -0,0 +1,29 @@
+function rede_social() {
+
+    // mostrar = document.getElementById('f1')
+    mostrar = document.getElementsByClassName('f2')
+    mostrar[0].style.display = "none"
+
+}
+
+function rede_socialoff() {
+
+    // mostrar = document.getElementById('f1')
+    mostrar = document.getElementsByClassName('f2')
+    mostrar[0].style.display = "block"
+}
+
+function rede_social1() {
+
+    // mostrar = document.getElementById('f1')
+    mostrar = document.getElementsByClassName('f2')
+    mostrar[1].style.display = "none"
+
+}
+
+function rede_socialoff1() {
+
+    // mostrar = document.getElementById('f1')
+    mostrar = document.getElementsByClassName('f2')
+    mostrar[1].style.display = "block"
+}
